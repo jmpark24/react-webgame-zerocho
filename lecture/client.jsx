e41072ffbca53c47459a1240d4bc2b3ac67b2b08
jmpark24 <stylack@gmail.com>
2023-12-18 15:00:17 +0900

    로또 게임 완성

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import RSP  from './RSP';
+import Lotto  from './Lotto';
 
 
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
-root.render(<RSP />);
+root.render(<Lotto />);
